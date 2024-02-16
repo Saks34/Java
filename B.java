29e5fed5c483b6caa0b8e3ab3e68f36966f853ec
Saksham <sakshamsatnalika34@gmail.com>
2024-02-16 16:47:25 +0530

    modified:   B.java

--- B.java
+++ B.java
@@ -2,6 +2,8 @@ public class B{
     public static void main(String[] args){
         String st = "hello";
         String a = "HELLO";
+        String s = "     HELLO    ";
+        String b = String.copyValueOf(new char[] {'h','e'});
         System.out.println(st);
         System.out.println(st.codePointBefore(1));
         System.out.println(st.codePointAt(1));
@@ -9,5 +11,21 @@ public class B{
         System.out.println(st.compareTo(a));
         System.out.println(st.compareToIgnoreCase(a));
         System.out.println(st.concat(a));
+        System.out.println(st.contains(a));
+        System.out.println(st.contentEquals(a));
+        System.out.println(b);
+        System.out.println(st.endsWith(a));
+        System.out.println(st.isEmpty());
+        System.out.println(st.lastIndexOf(4));
+        System.out.println(st.length());
+        System.out.println(st.matches(b));
+        System.out.println(st.substring(2));
+        System.out.println(st.toCharArray());
+        System.out.println(st.toLowerCase());
+        System.out.println(st.toString());
+        System.out.println(st.toUpperCase());
+        System.out.println(s.trim());
+        System.out.println(String.valueOf(5));
+
     }
 }
