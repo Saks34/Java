4de79a36336c00993d7edec666f91b0edcac1eed
Saks34 <sakshamsatnalika34@gmail.com>
2025-01-30 09:31:54 +0530

    29th jan

--- cf.java
+++ cf.java
@@ -1,13 +1,13 @@
 import java.util.*;
 
-public class ce {
+public class cf {
     public static int check(String a,int b, int z){
         if(b==a.length()){
             return z;
         }
        
         char p = a.charAt(b);
-        if(p=='a'||p=='e'||p=='i'||p=='u'||p=='o'){
+        if(p>='a'&& p<='z'){
             z++;
         }
         return check(a,b+1,z);
@@ -15,7 +15,7 @@ public class ce {
     public static void main(String[] ars){
         Scanner s = new Scanner(System.in);
         String z = s.nextLine();
-        z = z.toLowerCase();
+        
         int a = check(z, 0, 0);
         System.out.println(a);
     }
